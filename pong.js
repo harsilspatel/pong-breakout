53d4a3b381cf3829a1c5d97d7f5bff210b0b9647
hpat0003 <hpat0003@student.monash.edu>
2018-09-06 22:50:38 +1000

    even imporved resseting

--- pong.js
+++ pong.js
@@ -1,7 +1,7 @@
 "use strict";
 function pong() {
-    var speed = 1, fps = 2, score1 = 0, score2 = 0, gameRounds = 3;
-    const svg = document.getElementById("canvas");
+    let score1 = 0, score2 = 0, speed = 1, fps = 4;
+    const gameRounds = 3, svg = document.getElementById("canvas");
     let leftPaddle = new Elem(svg, 'rect')
         .attr('x', 10)
         .attr('y', 80)
@@ -22,27 +22,26 @@ function pong() {
         .attr('fill', '#FFFFFF')
         .attr('xSpeed', speed)
         .attr('ySpeed', speed);
-    const mainInterval = Observable.interval(fps)
-        .map(() => ({
-        x: parseInt(ball.attr('cx')),
-        y: parseInt(ball.attr('cy')),
-        r: parseInt(ball.attr('r'))
-    }));
+    const mainInterval = Observable.interval(fps);
     const mainObservable = mainInterval
-        .takeUntil(mainInterval.filter(({ x, y, r }) => score1 == gameRounds || score2 == gameRounds))
-        .map(() => ({
+        .takeUntil(mainInterval.filter(_ => score1 == gameRounds || score2 == gameRounds))
+        .map(_ => ({
         x: parseInt(ball.attr('cx')),
         y: parseInt(ball.attr('cy')),
         r: parseInt(ball.attr('r')),
         xSpeed: parseInt(ball.attr('xSpeed')),
         ySpeed: parseInt(ball.attr('ySpeed')),
+        score1,
+        score2,
+        gameRounds
     }));
     mainObservable
         .map(({ x, y, r, xSpeed, ySpeed }) => ({ x, y, r, xSpeed, ySpeed, paddleHeight: parseInt(rightPaddle.attr('height')), paddleWidth: parseInt(rightPaddle.attr('width')) }))
         .filter(({ x, y, r, xSpeed, ySpeed, paddleHeight, paddleWidth }) => (isCollision(x + r, parseInt(rightPaddle.attr('x')), xSpeed) &&
         (isBetween(y, parseInt(rightPaddle.attr('y')), paddleHeight, ySpeed))) ||
         (isCollision(x - r, parseInt(leftPaddle.attr('x')) + paddleWidth, xSpeed) &&
-            (isBetween(y, parseInt(leftPaddle.attr('y')), paddleHeight, ySpeed)))).subscribe(({ xSpeed }) => ball.attr('xSpeed', -1 * xSpeed));
+            (isBetween(y, parseInt(leftPaddle.attr('y')), paddleHeight, ySpeed))))
+        .subscribe(({ xSpeed }) => ball.attr('xSpeed', -1 * xSpeed));
     mainObservable.subscribe(({ x, y, xSpeed, ySpeed }) => ball.attr('cx', x + xSpeed).attr('cy', y + ySpeed));
     mainObservable
         .filter(({ y, r, ySpeed }) => isCollision(y - r, 0, ySpeed) || isCollision(y + r, Math.floor(svg.getBoundingClientRect().height), ySpeed))
@@ -52,27 +51,21 @@ function pong() {
         .filter(({ y, paddleHeight }) => isBetween(y, Math.floor(0 + paddleHeight / 2), Math.floor(svg.getBoundingClientRect().height - paddleHeight), 0))
         .subscribe(({ y, paddleHeight }) => leftPaddle.attr('y', y - Math.floor(paddleHeight / 2)));
     mainObservable
-        .map(({ x, xSpeed, r }) => isCollision(x - r, 0, xSpeed) ? updateAndReset(score1, ++score2, ball) : true)
-        .map(() => ({
-        x: parseInt(ball.attr('cx')),
-        y: parseInt(ball.attr('cy')),
-        r: parseInt(ball.attr('r'))
-    }))
-        .map(({ x, y, r }) => isCollision(x + r, Math.floor(svg.getBoundingClientRect().right) - Math.floor(svg.getBoundingClientRect().left), parseInt(ball.attr('xSpeed'))) ? updateAndReset(++score1, score2, ball) : true)
-        .map(_ => score1 == gameRounds || score2 == gameRounds ? endGame(score1, score2) : true)
-        .subscribe(_ => { });
+        .filter(({ x, xSpeed, r }) => isCollision(x - r, 0, xSpeed) || isCollision(x + r, Math.floor(svg.getBoundingClientRect().right) - Math.floor(svg.getBoundingClientRect().left), parseInt(ball.attr('xSpeed'))))
+        .map(({ x }) => (x < 0 ? scored(score1, ++score2, gameRounds) : scored(++score1, score2, gameRounds)))
+        .subscribe(_ => ball.attr('cx', getRandomBetween(400, 500)).attr('cy', getRandomBetween(250, 350)));
 }
-function endGame(score1, score2) {
-    const score = document.getElementById("score");
-    score2 == 3 ?
-        score.innerHTML = "Congratulations player2" :
-        score.innerHTML = "Congratulations player1";
-}
-function updateAndReset(score1, score2, ball) {
+function scored(score1, score2, gameRounds) {
     console.log('resetted the game!');
     const score = document.getElementById("score");
     score.innerHTML = `${score1} ${score2}`;
-    ball.attr('cx', getRandomBetween(400, 500)).attr('cy', getRandomBetween(250, 350));
+    const result = document.getElementById("result");
+    if (score2 == gameRounds) {
+        result.innerHTML = "Congratulations player 2 🤩";
+    }
+    else if (score1 == gameRounds) {
+        result.innerHTML = "Congratulations player 1 😎";
+    }
 }
 function controlPaddleObservable(paddle) {
     const svg = document.getElementById("canvas"), svgTop = Math.floor(svg.getBoundingClientRect().top), o = Observable
--- pong.ts
+++ pong.ts
@@ -13,14 +13,15 @@ function pong() {
   // Explain which ideas you have used ideas from the lectures to 
   // create reusable, generic functions.
 
-  var
-    speed = 1,
-    fps = 2,
-  score1 = 0,
+  let
+    score1 = 0,
     score2 = 0,
-    gameRounds = 3;
+    speed = 1,
+    fps = 4;
+  const
+    gameRounds = 3,
+    svg = document.getElementById("canvas")!;
 
-  const svg = document.getElementById("canvas")!;
   let leftPaddle = new Elem(svg, 'rect')
     .attr('x', 10)
     .attr('y', 80)
@@ -45,35 +46,27 @@ function pong() {
   .attr('xSpeed', speed)
   .attr('ySpeed', speed);
 
-  const mainInterval = Observable.interval(fps)
-  .map(() => ({
-    x: parseInt(ball.attr('cx')),
-    y: parseInt(ball.attr('cy')),
-    r: parseInt(ball.attr('r'))
-  }));
+  const mainInterval = Observable.interval(fps);
+  // .map(() => ({
+  //   score1: 0,
+  //   score2: 0,
+  //   gameRounds: 3
+  // }));
 
   const mainObservable = mainInterval
-    .takeUntil(mainInterval.filter(({x,y,r}) => score1 == gameRounds || score2 == gameRounds))
-    .map(() => ({
+  // {score1,score2,gameRounds}
+    .takeUntil(mainInterval.filter(_ => score1 == gameRounds || score2 == gameRounds))
+    .map(_ => ({
       x: parseInt(ball.attr('cx')),
       y: parseInt(ball.attr('cy')),
       r: parseInt(ball.attr('r')),
       xSpeed: parseInt(ball.attr('xSpeed')),
       ySpeed: parseInt(ball.attr('ySpeed')),
-      // paddleHeight: parseInt(rightPaddle.attr('height')), //both paddles have same height and widths
-      // paddleWidth: parseInt(rightPaddle.attr('width'))
+      score1,
+      score2,
+      gameRounds
     }));
 
-    // making the ball collide the paddles
-  // mainObservable
-  //   .map(({x,y,r, xSpeed, ySpeed}) => //if x coordinates for ball and right paddle are same and cy is between the edges of paddle then reverse the direction
-  //     (isCollision(x+r, parseInt(rightPaddle.attr('x')), xSpeed) && 
-  //     (isBetween(y, parseInt(rightPaddle.attr('y')), parseInt(rightPaddle.attr('height')), ySpeed))) ||
-  //   (isCollision(x-r, parseInt(leftPaddle.attr('x')) + parseInt(leftPaddle.attr('width')), xSpeed) &&
-  //     (isBetween(y, parseInt(leftPaddle.attr('y')), parseInt(leftPaddle.attr('height')), ySpeed)) // no need to check this since the left paddle is just following it
-  //   ) ? 
-  //         ball.attr('xSpeed', reverseDirection(ball, 'xSpeed')) : xSpeed )
-  //   .subscribe(() => (ball.attr('cx', parseInt(ball.attr('xSpeed'))+parseInt(ball.attr('cx')))))
 
     
     // making the ball collide the paddles
@@ -82,9 +75,9 @@ function pong() {
   .filter(({x,y,r, xSpeed, ySpeed, paddleHeight, paddleWidth}) => //if x coordinates for ball and right paddle are same and cy is between the edges of paddle then reverse the direction
     (isCollision(x+r, parseInt(rightPaddle.attr('x')), xSpeed) && 
     (isBetween(y, parseInt(rightPaddle.attr('y')), paddleHeight, ySpeed))) ||
-  (isCollision(x-r, parseInt(leftPaddle.attr('x')) + paddleWidth, xSpeed) &&
-    (isBetween(y, parseInt(leftPaddle.attr('y')), paddleHeight, ySpeed)) // no need to check this since the left paddle is just following it
-  )).subscribe(({xSpeed}) => ball.attr('xSpeed', -1*xSpeed))
+    (isCollision(x-r, parseInt(leftPaddle.attr('x')) + paddleWidth, xSpeed) &&
+    (isBetween(y, parseInt(leftPaddle.attr('y')), paddleHeight, ySpeed)))) // no need to check this since the left paddle is just following it
+  .subscribe(({xSpeed}) => ball.attr('xSpeed', -1*xSpeed))
 
   mainObservable.subscribe(({x, y, xSpeed, ySpeed}) => ball.attr('cx', x+xSpeed).attr('cy',y+ySpeed))
 
@@ -92,48 +85,49 @@ function pong() {
   mainObservable
     .filter(({y,r,ySpeed}) =>  isCollision(y-r, 0, ySpeed) || isCollision(y+r, Math.floor(svg.getBoundingClientRect().height), ySpeed))
     .subscribe(({ySpeed}) => ball.attr('ySpeed', -1*ySpeed))
-    // .subscribe(({}) => (ball.attr('cy', parseInt(ball.attr('ySpeed'))+parseInt(ball.attr('cy')))))
 
 
     // making left paddle track the ball 
   mainObservable
-    // .filter(({y}) => 0 <= y - parseInt(leftPaddle.attr('height'))/2 && y + parseInt(leftPaddle.attr('height'))/2 <= Math.floor(svg.getBoundingClientRect().height))
     .map(({y}) => ({y, paddleHeight:parseInt(rightPaddle.attr('height'))}))
     .filter(({y,paddleHeight}) => isBetween(y, Math.floor(0+paddleHeight/2), Math.floor(svg.getBoundingClientRect().height - paddleHeight),0))
     .subscribe(({y, paddleHeight}) => leftPaddle.attr('y', y - Math.floor(paddleHeight/2)))
 
-  //   // making the ball collide the top and bottom boundaries
-  // mainObservable
-  //   .map(({y,r,ySpeed}) =>  isCollision(y-r, 0, ySpeed) || isCollision(y+r, Math.floor(svg.getBoundingClientRect().height), ySpeed) ? ball.attr('ySpeed', reverseDirection(ball, 'ySpeed')) : ySpeed)
-  //   .subscribe(({}) => (ball.attr('cy', parseInt(ball.attr('ySpeed'))+parseInt(ball.attr('cy')))))
+// resetting the game 
+mainObservable
+.filter(({x,xSpeed,r}) => isCollision(x-r, 0, xSpeed) || isCollision(x+r, Math.floor(svg.getBoundingClientRect().right) - Math.floor(svg.getBoundingClientRect().left), parseInt(ball.attr('xSpeed'))))
+// .map(({x,score1,score2}) => document.getElementById("score")!.innerHTML = (x < 0 ? `${score1} ${++score2}` : `${++score1} ${score2}`))
+// .map(({score1,score2,gameRounds}) => document.getElementById("score")!.innerHTML = ())
+.map(({x}) => (x < 0 ? scored(score1,++score2,gameRounds) : scored(++score1,score2,gameRounds))) 
+.subscribe(_ => ball.attr('cx', getRandomBetween(400,500)).attr('cy', getRandomBetween(250,350)))
 
-
-
-    // resetting the game 
-  mainObservable
-  .map(({x,xSpeed,r}) => isCollision(x-r, 0, xSpeed) ? updateAndReset(score1, ++score2, ball) : true)
-  .map(() => ({
-    x: parseInt(ball.attr('cx')),
-    y: parseInt(ball.attr('cy')),
-    r: parseInt(ball.attr('r'))
-  }))
-  .map(({x,y,r}) => isCollision(x+r, Math.floor(svg.getBoundingClientRect().right) - Math.floor(svg.getBoundingClientRect().left), parseInt(ball.attr('xSpeed'))) ? updateAndReset(++score1, score2, ball) : true)
-  .map(_ => score1 == gameRounds || score2 == gameRounds? endGame(score1, score2) : true)
-  .subscribe(_ => {})
 }
+    
 
-function endGame(score1: Number, score2: Number){
-  const score = document.getElementById("score")!
-  score2 == 3 ?
-    score.innerHTML = "Congratulations player2" :
-    score.innerHTML = "Congratulations player1"
-}
+//     // resetting the game 
+//   mainObservable
+//   .map(({x,xSpeed,r}) => isCollision(x-r, 0, xSpeed) ? updateAndReset(score1, ++score2, ball) : true)
+//   .map(() => ({
+//     x: parseInt(ball.attr('cx')),
+//     y: parseInt(ball.attr('cy')),
+//     r: parseInt(ball.attr('r'))
+//   }))
+//   .map(({x,y,r}) => isCollision(x+r, Math.floor(svg.getBoundingClientRect().right) - Math.floor(svg.getBoundingClientRect().left), parseInt(ball.attr('xSpeed'))) ? updateAndReset(++score1, score2, ball) : true)
+//   .map(_ => score1 == gameRounds || score2 == gameRounds? endGame(score1, score2) : true)
+//   .subscribe(_ => {})
+// }
 
-function updateAndReset(score1: Number, score2: Number, ball: Elem) {
+function scored(score1: number, score2: number, gameRounds: number) {
   console.log('resetted the game!')
   const score = document.getElementById("score")!;
   score.innerHTML = `${score1} ${score2}`;
-  ball.attr('cx', getRandomBetween(400,500)).attr('cy', getRandomBetween(250,350))
+
+  const result = document.getElementById("result")!
+  if (score2 == gameRounds) {
+    result.innerHTML = "Congratulations player 2 🤩"
+  } else if (score1 == gameRounds) {
+    result.innerHTML = "Congratulations player 1 😎"
+  }
 }
 
 function controlPaddleObservable(paddle: Elem): void {
